4e6d2ec345face7cfa3fbeeca0d5f1204c85c447
ralphdosreis <ralphdosreistorres@gmail.com>
2024-04-28 10:57:35 -0300

    refatorado scroll-suave

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
-import initScrollSuave from './modules/scroll-suave.js';
+import ScrollSuave from './modules/scroll-suave.js';
 import animaMouseMove from './modules/vanila-tilt.js';
 import MenuMobile from './modules/menu-mobile.js';
 
-initScrollSuave();
+
 animaMouseMove();
+
+const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
+scrollSuave.init();
 
 const menuMobile = new MenuMobile('[data-menu="button"]', '[data-menu="list"]');
 menuMobile.init();
