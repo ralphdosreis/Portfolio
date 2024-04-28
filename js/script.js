44d7b527f318aa2391a04632d0959aa0fe51644c
ralphdosreis <ralphdosreistorres@gmail.com>
2024-04-28 09:10:40 -0300

    feito menu-mobile.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 import initScrollSuave from './modules/scroll-suave.js';
 import animaMouseMove from './modules/vanila-tilt.js';
+import MenuMobile from './modules/menu-mobile.js';
 
 initScrollSuave();
 animaMouseMove();
+const menuMobile = new MenuMobile('[data-menu="button"]', '[data-menu="list"]');
+menuMobile.init();
